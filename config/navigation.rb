d9483ace8257b72030c91bcf66cad764b9b58fc9
Thomas Hebb <tommyhebb@gmail.com>
2013-07-17 14:07:43 -0400

    Use lambda instead of Proc.new

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,15 +1,15 @@
 SimpleNavigation::Configuration.run do |navigation|
-  logged_in_proc = Proc.new {logged_in?}
+  logged_in_check = lambda {logged_in?}
 
   navigation.items do |primary|
     primary.dom_class = 'nav'
     primary.selected_class = 'active'
     primary.item :signup, 'Sign Up', url('/signup'),
-                 :unless => logged_in_proc
+                 :unless => logged_in_check
     primary.item :dashboard, 'Dashboard', url('/dashboard'),
-                 :if => logged_in_proc
+                 :if => logged_in_check
     primary.item :leaderboard, 'Leaderboard', url('/leaderboard'),
-                 :if => Proc.new {game_started?}
+                 :if => lambda {game_started?}
     primary.item :rules, 'Rules', url('/rules')
     primary.item :contact, 'Contact Us', url('/contact')
   end
